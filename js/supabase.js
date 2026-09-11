38712fa07cce1483e58f11984735f33bb892b830
Ujjwal Mishra <ujjwalmishrajee882007@gmail.com>
2026-09-11 10:30:00 +0530

    Nothing

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://zoqmoivwwmtxyrhkdqen.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_dBDBzbedRBsRblcAhBbEkw_QlPQAWP0';
 
-const supabaseClient = window.supabaseClient.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
