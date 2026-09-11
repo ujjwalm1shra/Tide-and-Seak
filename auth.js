78301aec8112876cd24a9fbf84550d348da12193
Ujjwal Mishra <ujjwalmishrajee882007@gmail.com>
2026-09-11 11:29:47 +0530

    Whatever

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ if (loginForm) {
         const email = document.getElementById("l-email").value;
         const password = document.getElementById("l-password").value;
 
-        const { data, error } = await supabaseClient.auth.signInWithPassword({
+        const { data, error } = await window.tideSupabaseClient.auth.signInWithPassword({
             email: email,
             password: password
         });
@@ -32,7 +32,7 @@ if (signupForm) {
         const email = document.getElementById('s-email').value;
         const password = document.getElementById('s-password').value;
 
-        const { data, error } = await supabaseClient.auth.signUp({
+        const { data, error } = await window.tideSupabaseClient.auth.signUp({
             email: email,
             password: password
         });
